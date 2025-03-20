7d8c4049cd7549a3165b6357b7fa103e512a25f8
Manas Tankala <EmailtoManas004@gmail.com>
2025-03-20 17:51:56 -0400

    first commit

---
+++ plan.md
@@ -0,0 +1,25 @@
+# Tiny Web Search Engine
+## Search Index
+- add new pages
+    AS A user,
+    I WANT to be able to add new web pages to the search engine, 
+    SO THAT I can expand its knowledge base
+
+- update existing pages
+    AS A user,
+    I WANT the search engine to update its information when a page's content changes
+    SO THAT the search results remain accurate
+- remove outdated content
+    AS A user,
+    I WANT the search engine to remove outdated or irrelevant web pages, 
+    SO THAT the search results are not cluttered or inconsistent.
+
+
+- retrieve info
+    AS A user,
+    I WANT to be able to search for information using keywords
+    SO THAT I can find relevant web pages
+## Search Algorithm
+## Ranking Algorithm
+## Spider
+## Command Line Interface
---
+++ tests/searchIndex.test.js
@@ -0,0 +1,42 @@
+const {
+  //object destructuring syntax
+  createIndex,
+  addPageToIndex,
+  updatePageInIndex,
+  removePageFromIndex,
+  getPagesForKeyword,
+} = require("../searchIndex");
+// const wholeImport = require("../searchIndex") // the long way without destructuring
+// const createIndexTheLongDeclaration = wholeImport.createIndex;
+// console.log(wholeImport);
+
+describe("Search Index", () => {
+  let index;
+
+  beforeEach(() => {
+    index = createIndex();
+  });
+
+  it("should add a new page to the index", () => {
+    //arrange
+    const exampleUrl = "https://www.example.com";
+    //act
+    addPageToIndex(index, exampleUrl, "This is a sample web page about dogs");
+    //assert
+    expect(getPagesForKeyword(index, "dogs")).toContain(exampleUrl);
+  });
+  it("should update a page in the index", () => {
+    //arrange
+    const exampleUrl = "https://www.example.com";
+    addPageToIndex(index, exampleUrl, "This is a sample web page about dogs");
+    //act
+    updatePageInIndex(
+      index,
+      exampleUrl,
+      "This is a different web page, concerning cats"
+    );
+    //assert
+    expect(getPagesForKeyword(index, "dogs")).not.toContain(exampleUrl);
+    expect(getPagesForKeyword(index, "cats")).toContain(exampleUrl);
+  });
+});
