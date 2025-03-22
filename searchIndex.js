4a5cc4073d541b647404ca8e3b356d8d00e6b6e4
Manas Tankala <EmailtoManas004@gmail.com>
2025-03-21 23:06:09 -0400

    wraps basic scenarios and begins exploring test cases

--- searchIndex.js
+++ searchIndex.js
@@ -1,7 +1,5 @@
 function createIndex() {
-  return {
-    
-  }; // Using an object as the index
+  return {};
 }
 
 function addPageToIndex(index, url, pageContent) {
@@ -11,22 +9,6 @@ function addPageToIndex(index, url, pageContent) {
 
   allKeywords.forEach((keyword) => {
     if (!index[keyword]) {
-      index[keyword] = [];
-    }
-    if (!index[keyword].includes(url)) {
-      index[keyword].push(url);
-    }
-  });
-}
-function updatePageInIndex(index, url, pageContent) {
-  for (const keyword in index) {
-    if (index[keyword]) {
-      index[keyword] = index[keyword].filter((u) => u !== url);
-    }
-  }
-  const keywords = pageContent.toLowerCase().split(" ");
-  keywords.forEach((keyword) => {
-    if (!index[keyword]) {
       index[keyword] = new Set();
     }
     index[keyword].add(url);
@@ -34,60 +16,29 @@ function updatePageInIndex(index, url, pageContent) {
 }
 
 function updatePageInIndex(index, url, pageContent) {
-  for (const keyword in index) {
-    index[keyword].delete(url);
-  }
-
+  removePageFromIndex(index, url);
   addPageToIndex(index, url, pageContent);
 }
 
 function removePageFromIndex(index, url) {
-
-}
-  if (!index || !url) {
-<<<<<<< HEAD
-    return;
-  }
   for (const keyword in index) {
-    
-  }
-    if (index.hasOwnProperty(keyword) && index[keyword]) {
-      (index[keyword] = index[keyword]), filter((u) => u !== url);
-      if (index[keyword].length === 0) {
+    if (index[keyword].has(url)) {
+      index[keyword].delete(url);
+      if (index[keyword].size === 0) {
         delete index[keyword];
-    return; // Corrected: return is now inside the if block
-  }
-
-  for (const keyword in index) {
-    if (index.hasOwnProperty(keyword)) {
-      if (index[keyword] && index[keyword].delete) {
-        index[keyword].delete(url);
-
-        if (index[keyword].size === 0) {
-          delete index[keyword];
-        }
       }
     }
   }
 }
 
 function getPagesForKeyword(index, keyword) {
-  // if (keyword in index) {
-  //  return index[keyword];
-  // } else {
-  // return [];
-  // }
-  for (key in index) {
-    console.log(key);
-  }
-  return index[keyword] || [];
+  return index[keyword];
 }
 
 function extractKeywords(pageContent) {
   let words = pageContent.toLowerCase().split(/[^a-z0-9]+/);
   const stopWords = ["the", "is", "and", "a", "an", "in", "on", "at", "by"];
   return words.filter((word) => word.length && !stopWords.includes(word));
-  return index[keyword];
 }
 
 module.exports = {
--- tests/searchIndex.test.js
+++ tests/searchIndex.test.js
@@ -1,51 +1,70 @@
 const {
-  //object destructuring syntax
   createIndex,
   addPageToIndex,
   updatePageInIndex,
   removePageFromIndex,
   getPagesForKeyword,
 } = require("../searchIndex");
-// const wholeImport = require("../searchIndex") // the long way without destructuring
-// const createIndexTheLongDeclaration = wholeImport.createIndex;
-// console.log(wholeImport);
 
-describe("Search Index", () => {});
-let index;
+describe("Search Index", () => {
+  let index;
 
-beforeEach(() => {
-  index = createIndex();
-});
+  beforeEach(() => {
+    index = createIndex();
+  });
 
-it.only("should add a new page to the index", () => {
-  //arrange
-  const exampleUrl = "https://www.example.com";
-  //act
-  addPageToIndex(
-    index,
-    exampleUrl,
-    "This is a sample web page about dogs. This web page talks about dogs a lot. dogs"
-  );
   it("should add a new page to the index", () => {
-    //arrange
+    // Scenarios 1 & 4
     const exampleUrl = "https://www.example.com";
-    //act
-    addPageToIndex(index, exampleUrl, "This is a sample web page about dogs");
-    //assert
+    addPageToIndex(
+      index,
+      exampleUrl,
+      "This is a sample web page about dogs. This web page talks about dogs a lot. dogs"
+    );
     expect(getPagesForKeyword(index, "dogs")).toContain(exampleUrl);
   });
-  it("should update a page in the index", () => {
-    //arrange
+
+  it("should add a new page to the index", () => {
     const exampleUrl = "https://www.example.com";
     addPageToIndex(index, exampleUrl, "This is a sample web page about dogs");
-    //act
+    expect(getPagesForKeyword(index, "dogs")).toContain(exampleUrl);
+  });
+
+  it("should update a page in the index", () => {
+    const exampleUrl = "https://www.example.com";
+    addPageToIndex(index, exampleUrl, "This is a sample web page about dogs");
     updatePageInIndex(
       index,
       exampleUrl,
       "This is a different web page, concerning cats"
     );
-    //assert
-    expect(getPagesForKeyword(index, "dogs")).not.toContain(exampleUrl);
+    expect(getPagesForKeyword(index, "dogs")).toBeUndefined();
     expect(getPagesForKeyword(index, "cats")).toContain(exampleUrl);
+  });
+  it("should remove a page from the index", () => {
+    const exampleUrl = "https://www.example.com";
+    addPageToIndex(index, exampleUrl, "This is a sample web page about dogs");
+    removePageFromIndex(index, exampleUrl);
+    expect(getPagesForKeyword(index, "dogs")).toBeUndefined();
+  });
+  it("should remove only one page from the index", () => {
+    //edge case, deleting too many keywords
+    const lionsUrl = "https://www.lions.com";
+    const tigersUrl = "https://www.tigers.com";
+    addPageToIndex(index, lionsUrl, "a lion is a cat");
+    addPageToIndex(index, tigersUrl, "a tiger is a cat");
+    removePageFromIndex(index, lionsUrl);
+
+    const lionsPages = getPagesForKeyword(index, "lion");
+    expect(lionsPages ? lionsPages.size : 0).toBe(0);
+
+    const tigersPages = getPagesForKeyword(index, "tiger");
+    expect(tigersPages ? tigersPages.size : 0).toBe(1);
+  });
+  it("should match keywords regardless of their case", () => {
+    const catsUrl = "thecatsurl";
+    addPageToIndex(index, catsUrl, "Old Deuteronomy is from CATS");
+    const results = getPagesForKeyword(index, "cats");
+    expect(results).toContain(catsUrl);
   });
 });
