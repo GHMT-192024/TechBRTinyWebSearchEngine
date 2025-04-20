928dd67e067c9625da84fbd67c49a2332e50158e
GHMT-192024 <EmailtoManas004@gmail.com>
2025-04-20 14:22:08 -0400

    Delete fresh-project/tests/searchIndex.test.js

--- fresh-project/tests/searchIndex.test.js
+++
@@ -1,9 +0,0 @@
-console.log();
-const { createIndex } = require("../src/searchIndex.js");
-
-describe("Search Index", () => {
-  it("should create an index", () => {
-    const index = createIndex();
-    expect(index).toBeTruthy();
-  });
-});
