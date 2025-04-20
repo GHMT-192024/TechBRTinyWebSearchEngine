1fdd94ab1e46fe339da438ff46027287d0d4918f
GHMT-192024 <EmailtoManas004@gmail.com>
2025-04-20 14:21:51 -0400

    Delete fresh-project/tests/myModule.test.js

--- fresh-project/tests/myModule.test.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const myModulePath = path.resolve(__dirname, "../src/myModule.js"); // Go up, then into src
-const { myFunction } = require(myModulePath);
-
-describe("My Test", () => {
-  it("should run", () => {
-    expect(myFunction()).toBe("Hello");
-  });
-});
